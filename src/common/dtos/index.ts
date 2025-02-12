0995e55f3c9a0550b3f91ef2d5882c35ea3aec65
linhprovip2002 <dangquangnhatlinh123@gmail.com>
2025-02-12 13:47:32 +0700

    fix: fix conflict

--- src/common/dtos/index.ts
+++ src/common/dtos/index.ts
@@ -1,3 +1,6 @@
 import { ResponseDto } from './response.dto';
+import { PageDto } from './page.dto';
+import { PageMetaDto } from './page-meta.dto';
+import { PaginationDto } from './page-option.dto';
 
-export { ResponseDto };
+export { ResponseDto, PageDto, PageMetaDto, PaginationDto };
--- src/types/index.ts
+++
@@ -1,6 +0,0 @@
-import { SocialOwnerType } from './common-enums';
-import { OrganizationType } from './common-enums';
-import { SocialType } from './common-enums';
-import { UserStatus } from './common-enums';
-
-export { SocialOwnerType, OrganizationType, SocialType, UserStatus };

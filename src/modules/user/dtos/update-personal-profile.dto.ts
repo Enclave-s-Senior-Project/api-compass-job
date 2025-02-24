7cfd4e67d01311ee00a79ffbad9ca760668a5d65
hoaian03IT <dangtranhoaian8603@gmail.com>
2025-02-24 22:08:36 +0700

    chore: upload image to imagekit

--- src/modules/user/dtos/update-personal-profile.dto.ts
+++ src/modules/user/dtos/update-personal-profile.dto.ts
@@ -1,36 +1,19 @@
 import { UpdatePersonalProfileDtoErrorType } from '@common/errors/class-validator-error-type';
-import { ApiProperty } from '@nestjs/swagger';
-import { IsOptional, IsString, Matches } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString, Matches } from 'class-validator';
 
 export class UpdatePersonalProfileDto {
-    @ApiProperty({
-        description: 'Brief description about education',
-        default: `
-        <ul>
-            <li><strong>Formal Education:</strong> Structured learning in schools, colleges, and universities.</li>
-            <li><strong>Informal Education:</strong> Learning through life experiences, self-study, and mentorship.</li>
-            <li><strong>Non-formal Education:</strong> Skill-based training outside traditional schools, such as workshops and online courses.</li>
-        </ul>`,
-    })
-    @IsString()
-    @IsOptional()
-    readonly education?: string;
+    @IsString({ message: UpdatePersonalProfileDtoErrorType.PROFILE_URL_REQUIRED })
+    @IsNotEmpty({ message: UpdatePersonalProfileDtoErrorType.PROFILE_URL_STRING })
+    readonly profileUrl: string;
 
-    @ApiProperty({
-        description: 'Brief description about experience',
-        default: `
-        <ul>
-            <li><strong>Formal Education:</strong> Structured learning in schools, colleges, and universities.</li>
-            <li><strong>Informal Education:</strong> Learning through life experiences, self-study, and mentorship.</li>
-            <li><strong>Non-formal Education:</strong> Skill-based training outside traditional schools, such as workshops and online courses.</li>
-        </ul>`,
-    })
-    @IsString()
-    @IsOptional()
-    readonly experience?: string;
+    @IsString({ message: UpdatePersonalProfileDtoErrorType.PROFILE_URL_REQUIRED })
+    @IsNotEmpty({ message: UpdatePersonalProfileDtoErrorType.PROFILE_URL_STRING })
+    readonly pageUrl: string;
 
+    readonly education: string;
+    readonly experience: string;
+
+    @IsOptional()
     @Matches(/^\+(?:[0-9]\x20?){6,14}[0-9]$/, { message: UpdatePersonalProfileDtoErrorType.PHONE_NUMBER_INVALID })
-    @IsString()
-    @IsOptional()
-    readonly phone?: string;
+    readonly phone: string;
 }

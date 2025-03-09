7d2d2a56c5925f756b88707cd2a40820f055b803
hoaian03IT <dangtranhoaian8603@gmail.com>
2025-03-09 23:54:37 +0700

    fix: return redirect to callback page of client

--- src/modules/auth/oauth2/facebook/facebook.controller.ts
+++ src/modules/auth/oauth2/facebook/facebook.controller.ts
@@ -3,11 +3,15 @@ import { CurrentUser, SkipAuth } from '../../decorators';
 import { OAuth2Service } from '../services/oauth2.service';
 import { FacebookOAuth2Guard } from '../guards/facebook-oauth2.guard';
 import { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
 
 @Controller({ path: 'auth/facebook', version: '1' })
 @SkipAuth()
 export class FacebookController {
-    constructor(private readonly oauth2Service: OAuth2Service) {}
+    constructor(
+        private readonly oauth2Service: OAuth2Service,
+        private readonly configService: ConfigService
+    ) {}
 
     @UseGuards(FacebookOAuth2Guard)
     @Get()
@@ -17,7 +21,7 @@ export class FacebookController {
 
     @UseGuards(FacebookOAuth2Guard)
     @Get('callback')
-    async facebookCallback(@CurrentUser() user, @Res() res: Response) {
+    async facebookCallback(@CurrentUser() user, @Res({ passthrough: true }) res: Response) {
         const { builder, refreshToken, refreshTokenExpires } = await this.oauth2Service.oauth2Login(user);
         res.cookie('refresh-token', refreshToken, {
             httpOnly: true,
@@ -26,6 +30,12 @@ export class FacebookController {
             maxAge: refreshTokenExpires,
         });
 
-        return builder;
+        const query = new URLSearchParams({
+            tokenType: builder.value.tokenType,
+            accessToken: builder.value.accessToken,
+            accessTokenExpires: builder.value.accessTokenExpires,
+        }).toString();
+
+        res.redirect(`${this.configService.get<string>('CLIENT_URL_CALLBACK')}?${query}`);
     }
 }

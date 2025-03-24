01f313274a31aac11e025fb44e46d5dcbf4a6bb6
Quannisme <quanismevn@gmail.com>
2025-03-24 10:22:48 +0700

    feat: api list candidate apply job

--- src/modules/apply-job/apply-job.controller.ts
+++ src/modules/apply-job/apply-job.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, UseGuards } from '@nestjs/common';
+import { Pagination } from './../../libs/pagination/pagination.helper';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, UseGuards, Query } from '@nestjs/common';
 import { ApplyJobService } from './services/apply-job.service';
 import { CreateApplyJobDto } from './dtos/create-apply-job.dto';
 import { UpdateApplyJobDto } from './dtos/update-apply-job.dto';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
-import { CurrentUser, TOKEN_NAME } from '@modules/auth';
+import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { CurrentUser, SkipAuth, TOKEN_NAME } from '@modules/auth';
 import { RolesGuard } from '@modules/auth/guards/role.guard';
 import { Role, Roles } from '@modules/auth/decorators/roles.decorator';
-import { JwtPayload } from '@common/dtos';
+import { JwtPayload, PaginationDto } from '@common/dtos';
+import { ApplyJobResponseDto } from './dtos';
 
 @ApiTags('Apply-job')
 @Controller({ path: 'apply-job', version: '1' })
@@ -23,5 +25,15 @@ export class ApplyJobController {
     ): Promise<ApplyJobResponseDto> {
         return this.applyJobService.applyJob(createApplyJobDto, user);
     }
+
+    @SkipAuth()
+    @ApiOperation({ summary: 'Retrieve all candidates ' })
+    @ApiResponse({ status: 200, description: 'List of candidates applied job.' })
+    @Get(':id')
+    async listCandidatesApplyJob(
+        @Param('id') id: string,
+        @Query() pagination: PaginationDto
+    ): Promise<ApplyJobResponseDto> {
+        return this.applyJobService.listCandidatesApplyJob(id, pagination);
+    }
 }
-import { ApplyJobResponseDto } from './dtos';

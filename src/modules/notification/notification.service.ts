3cd15564eb467ded29a241cf84e028136a47f540
hoaian03IT <dangtranhoaian8603@gmail.com>
2025-04-10 15:34:43 +0700

    feat: notification apis

--- src/modules/notification/notification.service.ts
+++ src/modules/notification/notification.service.ts
@@ -8,6 +8,9 @@ import { CreateNotificationDto } from './dto/create-notification.dto';
 import { ErrorCatchHelper } from '@src/helpers/error-catch.helper';
 import { NotificationEntity } from '@src/database/entities';
 import { FcmTokenService } from '../fcm-token/fcm-token.service';
+import { PageDto, PageMetaDto, PaginationDto } from '@src/common/dtos';
+import { NotificationResponseDtoBuilder } from './dto/notification-response.dto';
+import { In } from 'typeorm';
 
 @Injectable()
 export class NotificationService {
@@ -72,6 +75,141 @@ export class NotificationService {
             this.fcmTokenService.deleteTokenWithoutAccountId(failedTokens);
 
             return response;
+        } catch (error) {
+            throw ErrorCatchHelper.serviceCatch(error);
+        }
+    }
+
+    public async getNotifications(accountId: string, pagination: PaginationDto) {
+        try {
+            const [notifications, total] = await this.notificationRepository.findAndCount({
+                where: {
+                    account: {
+                        accountId: accountId,
+                    },
+                },
+                relations: {
+                    account: true,
+                },
+                select: {
+                    notificationId: true,
+                    type: true,
+                    isRead: true,
+                    createdAt: true,
+                    link: true,
+                    message: true,
+                    title: true,
+                    account: {
+                        accountId: true,
+                    },
+                },
+                order: {
+                    createdAt: 'DESC',
+                },
+                skip: pagination.skip,
+                take: pagination.take,
+            });
+
+            const meta: PageMetaDto = new PageMetaDto({
+                itemCount: total,
+                pageOptionsDto: pagination,
+            });
+
+            return new NotificationResponseDtoBuilder()
+                .setValue(new PageDto<NotificationEntity>(notifications, meta))
+                .success()
+                .build();
+        } catch (error) {
+            throw ErrorCatchHelper.serviceCatch(error);
+        }
+    }
+
+    public async getNotificationById(accountId: string, notificationId: string) {
+        try {
+            const notification = await this.notificationRepository.findOne({
+                where: {
+                    notificationId: notificationId,
+                    account: {
+                        accountId: accountId,
+                    },
+                },
+                relations: {
+                    account: true,
+                },
+                select: {
+                    notificationId: true,
+                    type: true,
+                    isRead: true,
+                    createdAt: true,
+                    link: true,
+                    message: true,
+                    title: true,
+                    account: {
+                        accountId: true,
+                    },
+                },
+            });
+
+            return new NotificationResponseDtoBuilder().setValue(notification).success().build();
+        } catch (error) {
+            throw ErrorCatchHelper.serviceCatch(error);
+        }
+    }
+
+    public async hasNotification(accountId: string) {
+        try {
+            const result = await this.notificationRepository.exists({
+                where: {
+                    account: {
+                        accountId: accountId,
+                    },
+                    isRead: false,
+                },
+            });
+
+            return new NotificationResponseDtoBuilder().setValue(result).success().build();
+        } catch (error) {
+            throw ErrorCatchHelper.serviceCatch(error);
+        }
+    }
+
+    public async markAsRead(accountId: string, notificationIds: string[]) {
+        try {
+            const notifications = await this.notificationRepository.find({
+                where: {
+                    account: {
+                        accountId: accountId,
+                    },
+                    notificationId: In(notificationIds),
+                },
+            });
+
+            notifications.forEach((notification) => {
+                notification.isRead = true;
+            });
+
+            await this.notificationRepository.save(notifications);
+
+            return new NotificationResponseDtoBuilder().setValue(notifications).success().build();
+        } catch (error) {
+            throw ErrorCatchHelper.serviceCatch(error);
+        }
+    }
+
+    public async markAsReadAll(accountId: string) {
+        try {
+            await this.notificationRepository.update(
+                {
+                    account: {
+                        accountId: accountId,
+                    },
+                },
+                {
+                    isRead: true,
+                }
+            );
+
+            return new NotificationResponseDtoBuilder().setValue(null).success().build();
         } catch (error) {
             throw ErrorCatchHelper.serviceCatch(error);
         }
